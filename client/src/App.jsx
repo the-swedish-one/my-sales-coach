6bd68a944f90fe05989f6f15c0ab45efd6e3fc9e
the-swedish-one <a.damgaard@oxon.org>
2023-05-22 15:52:05 +0200

    Add typing and messages states

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,14 @@ import {
 } from "@chatscope/chat-ui-kit-react";
 
 function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT! I'm interested in buying a pen.",
+      sender: "ChatGPT",
+    },
+  ]);
+
   return (
     <div>
       <div style={{ position: "relative", height: "800px", width: "700px" }}>
