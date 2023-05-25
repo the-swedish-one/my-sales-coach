abd26879b39ccc2559f7f7f4f3f7d6c307f6ce6f
the-swedish-one <a.damgaard@oxon.org>
2023-05-25 17:31:29 +0200

    Add 2 mic icons for starting / stopping recording

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,10 +14,8 @@ import "regenerator-runtime";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-// import { useSpeechSynthesis } from "react-speech-kit";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import { convertTestToMp3 } from "my-sales-coach/routes/index.js";
 
 // import the api key here
 const chatGptAPI = import.meta.env.VITE_CHAT_GPT_API_KEY;
@@ -29,7 +27,7 @@ function App() {
   const [messages, setMessages] = useState([
     { message: "Hi there!", sender: "ChatGPT" },
   ]); // initial message in the chat window
-  const [systemMessageContent, setSystemMessageContent] = useState(""); // system message varibale for chatGPT priming
+  const [systemMessageContent, setSystemMessageContent] = useState(""); // system message variable for chatGPT priming
 
   // import the react speach recognition hook
   const {
@@ -39,9 +37,7 @@ function App() {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
-  // declaring a varible and assigning the value of the react useSpeechSynthesis hook
-  // const { speak } = useSpeechSynthesis();
-
+  // if the browser doesn't support speech recognition then display this on the page
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
@@ -121,6 +117,7 @@ function App() {
       ],
     };
 
+    // send the message to chat gpt and get a response back
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
@@ -133,8 +130,9 @@ function App() {
     const data = await response.json();
 
     // console.log(data);
-    // console.log(data.choices[0].message.content); // This is chatGPT's message back!
+    // console.log(data.choices[0].message.content); // <<<  This is chatGPT's message back!
 
+    // send chatgpt's text response to the googlecloud text-to-voice api via the backend call to googlecloud
     const audioResponse = await fetch("/api", {
       method: "POST",
       headers: {
@@ -143,6 +141,7 @@ function App() {
       body: JSON.stringify({ text: data.choices[0].message.content }),
     });
 
+    // set the messages with our previous messages plus chatGPT's reply
     setMessages([
       ...chatMessages,
       {
@@ -204,29 +203,83 @@ function App() {
         </MessageList>
       </div>
 
-      {/* button speaking text using the react useSpeechSynthesis hook */}
-      {/* <button onClick={() => speak({ text: "Hello" })}>Play Message</button> */}
-
       <textarea
         style={{ height: "100px", width: "500px" }}
         placeholder="Type your message here or use the buttons to record your voice!"
         onChange={handleInputChange}
         value={message || transcript}
       />
+
+      {/* Microphone icon for starting and stopping the recording */}
       <div>
-        <svg
+        {/* <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 384 512"
           style={{ height: "50px" }}
           onClick={SpeechRecognition.startListening}
         >
           <path d="M192 0C139 0 96 43 96 96V256c0 53 43 96 96 96s96-43 96-96V96c0-53-43-96-96-96zM64 216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 89.1 66.2 162.7 152 174.4V464H120c-13.3 0-24 10.7-24 24s10.7 24 24 24h72 72c13.3 0 24-10.7 24-24s-10.7-24-24-24H216V430.4c85.8-11.7 152-85.3 152-174.4V216c0-13.3-10.7-24-24-24s-24 10.7-24 24v40c0 70.7-57.3 128-128 128s-128-57.3-128-128V216z" />
-        </svg>
-        <p>Microphone: {listening ? "on" : "off"}</p>
-        <button onClick={handleSend}>Send!</button>
+        </svg> */}
+        {listening ? (
+          <svg
+            onClick={SpeechRecognition.stopListening}
+            style={{ height: "50px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            // xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="1080"
+            zoomAndPan="magnify"
+            viewBox="0 0 810 809.999993"
+            height="1080"
+            preserveAspectRatio="xMidYMid meet"
+            version="1.0"
+          >
+            <path
+              fill="#e80303"
+              d="M 404.859375 436.980469 C 438.515625 436.980469 465.890625 409.75 465.890625 376.089844 L 465.890625 249.5625 C 465.890625 215.90625 438.515625 188.535156 404.859375 188.535156 C 371.203125 188.535156 343.96875 215.90625 343.96875 249.5625 L 343.96875 376.089844 C 343.96875 409.609375 371.203125 436.980469 404.859375 436.980469 Z M 404.859375 436.980469 "
+              // fill-opacity="1"
+              // fill-rule="nonzero"
+            />
+            <path
+              fill="#e80303"
+              d="M 405 0 C 181.273438 0 0 181.273438 0 405 C 0 628.726562 181.273438 810 405 810 C 628.726562 810 810 628.726562 810 405 C 810 181.273438 628.726562 0 405 0 Z M 302.074219 249.5625 C 302.074219 192.722656 348.160156 146.636719 404.859375 146.636719 C 461.558594 146.636719 507.785156 192.863281 507.785156 249.5625 L 507.785156 376.089844 C 507.785156 432.792969 461.558594 478.878906 404.859375 478.878906 C 348.160156 478.878906 302.074219 432.792969 302.074219 376.089844 Z M 425.808594 530.132812 L 425.808594 621.464844 L 473.429688 621.464844 C 485.023438 621.464844 494.378906 630.824219 494.378906 642.414062 C 494.378906 654.003906 485.023438 663.363281 473.429688 663.363281 L 336.429688 663.363281 C 324.839844 663.363281 315.480469 654.003906 315.480469 642.414062 C 315.480469 630.824219 324.839844 621.464844 336.429688 621.464844 L 383.910156 621.464844 L 383.910156 530.132812 C 308.078125 519.796875 249.5625 454.855469 249.5625 376.089844 C 249.5625 364.5 258.921875 355.144531 270.511719 355.144531 C 282.101562 355.144531 291.460938 364.5 291.460938 376.089844 C 291.460938 438.796875 342.292969 489.769531 404.859375 489.769531 C 467.425781 489.769531 518.539062 438.796875 518.539062 376.089844 C 518.539062 364.5 527.894531 355.144531 539.488281 355.144531 C 551.078125 355.144531 560.4375 364.5 560.4375 376.089844 C 560.4375 454.71875 501.640625 519.796875 425.808594 530.132812 Z M 425.808594 530.132812 "
+              // fill-opacity="1"
+              // fill-rule="nonzero"
+            />
+          </svg>
+        ) : (
+          <svg
+            onClick={SpeechRecognition.startListening}
+            style={{ height: "50px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            // xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="1080"
+            zoomAndPan="magnify"
+            viewBox="0 0 810 809.999993"
+            height="1080"
+            preserveAspectRatio="xMidYMid meet"
+            version="1.0"
+          >
+            <path
+              fill="#000000"
+              d="M 404.859375 436.980469 C 438.515625 436.980469 465.890625 409.75 465.890625 376.089844 L 465.890625 249.5625 C 465.890625 215.90625 438.515625 188.535156 404.859375 188.535156 C 371.203125 188.535156 343.96875 215.90625 343.96875 249.5625 L 343.96875 376.089844 C 343.96875 409.609375 371.203125 436.980469 404.859375 436.980469 Z M 404.859375 436.980469 "
+              // fill-opacity="1"
+              // fill-rule="nonzero"
+            />
+            <path
+              fill="#000000"
+              d="M 405 0 C 181.273438 0 0 181.273438 0 405 C 0 628.726562 181.273438 810 405 810 C 628.726562 810 810 628.726562 810 405 C 810 181.273438 628.726562 0 405 0 Z M 302.074219 249.5625 C 302.074219 192.722656 348.160156 146.636719 404.859375 146.636719 C 461.558594 146.636719 507.785156 192.863281 507.785156 249.5625 L 507.785156 376.089844 C 507.785156 432.792969 461.558594 478.878906 404.859375 478.878906 C 348.160156 478.878906 302.074219 432.792969 302.074219 376.089844 Z M 425.808594 530.132812 L 425.808594 621.464844 L 473.429688 621.464844 C 485.023438 621.464844 494.378906 630.824219 494.378906 642.414062 C 494.378906 654.003906 485.023438 663.363281 473.429688 663.363281 L 336.429688 663.363281 C 324.839844 663.363281 315.480469 654.003906 315.480469 642.414062 C 315.480469 630.824219 324.839844 621.464844 336.429688 621.464844 L 383.910156 621.464844 L 383.910156 530.132812 C 308.078125 519.796875 249.5625 454.855469 249.5625 376.089844 C 249.5625 364.5 258.921875 355.144531 270.511719 355.144531 C 282.101562 355.144531 291.460938 364.5 291.460938 376.089844 C 291.460938 438.796875 342.292969 489.769531 404.859375 489.769531 C 467.425781 489.769531 518.539062 438.796875 518.539062 376.089844 C 518.539062 364.5 527.894531 355.144531 539.488281 355.144531 C 551.078125 355.144531 560.4375 364.5 560.4375 376.089844 C 560.4375 454.71875 501.640625 519.796875 425.808594 530.132812 Z M 425.808594 530.132812 "
+              // fill-opacity="1"
+              // fill-rule="nonzero"
+            />
+          </svg>
+        )}
       </div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={handleSend}>Send!</button>
 
-      {/* <div style={{ position: "relative", height: "400px", width: "500px" }}>
+      {/* div containing commented out code for chatgpt components */}
+      <div>
+        {/* <div style={{ position: "relative", height: "400px", width: "500px" }}>
         <MainContainer>
           <ChatContainer>
             <MessageList
@@ -248,6 +301,7 @@ function App() {
           </ChatContainer>
         </MainContainer>
       </div> */}
+      </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
