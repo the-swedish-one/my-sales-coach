4c1f4a7328162c8d9366594b9e810e868ae931ed
the-swedish-one <a.damgaard@oxon.org>
2023-05-25 16:37:41 +0200

    Add texttospeech method

---
+++ routes/texttospeech.js
@@ -0,0 +1,33 @@
+const textToSpeech = require("@google-cloud/text-to-speech");
+
+require("dotenv").config();
+
+const fs = require("fs");
+const util = require("util");
+
+const client = new textToSpeech.TextToSpeechClient();
+
+async function convertTextToMp3(text) {
+  // const text = "Hello world";
+  const request = {
+    input: { text: text },
+    voice: {
+      languageCode: "en-gb",
+      name: "en-GB-Neural2-A",
+      ssmlGender: "FEMALE",
+    },
+    audioConfig: { audioEncoding: "MP3" },
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+
+  const writeFile = util.promisify(fs.writeFile);
+
+  await writeFile("output.mp3", response.audioContent, "binary");
+
+  console.log("text to speech has completed");
+}
+
+// convertTextToMp3();
+
+module.exports = convertTextToMp3;
